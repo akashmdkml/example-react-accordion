dbccd99fc5ba56e864cd79f3925634baccaf720a
Trevor D. Miller <trevordmiller@icloud.com>
2015-09-05 13:30:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-accordion-example
+An example accordion component built with React for an [Egghead.io](https://egghead.io) lesson
+
+---
+
+## Getting Started
+
+1. Clone the project
+1. `npm install`
+1. `npm run dev`
---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const styles = {
+  active: {
+    display: 'inherit'
+  },
+  inactive: {
+    display: 'none'
+  }
+};
+
+class Accordion extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      active: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      active: !this.state.active
+    });
+  }
+
+  render() {
+    const stateStyle = this.state.active ? styles.active : styles.inactive;
+    return (
+      <div>
+        <a onClick={this.toggle}>
+          {this.props.summary}
+        </a>
+        <p style={stateStyle}>
+          {this.props.details}
+        </p>
+      </div>
+    );
+  }
+}
+
+Accordion.propTypes = {
+  summary: React.PropTypes.string.isRequired,
+  details: React.PropTypes.string.isRequired
+};
+
+export default Accordion;
